2d70a8bce7b5949b6273d95ed14cdb5841fe7c0b
jfjeffries <jon.f.jeffries@gmail.com>
2018-08-19 16:48:33 -0400

    added some crud

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,4 +1,6 @@
 ﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,22 +15,59 @@ namespace ElevenNote.WebMVC.Controllers
         // GET: Note
         public ActionResult Index()
         {
-            var model = new NoteListItem[0];
+            var service = CreateNoteService();
+            var model = service.GetNotes();
             return View(model);
         }
-        public ActionResult Create()
-        {
-            return View();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model)
         {
-            if (ModelState.IsValid)
-            {
+            if (!ModelState.IsValid) return View(model);
+            
+            var service = CreateNoteService();
 
+            if (service.CreateNote(model))
+            {
+                //ViewBag.SaveResult = "Your note was created.";
+                TempData["SaveResult"] = "Your note was created.";
+                return RedirectToAction("Index");
             }
+
+            ModelState.AddModelError("", "Note could not be created.");
+
+            return View(model);
+            
+        }
+
+        public ActionResult Details(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
             return View(model);
         }
+
+        public ActionResult Edit (int id)
+        {
+            var service = CreateNoteService();
+            var detail = service.GetNoteById(id);
+            var model =
+                new NoteEdit
+                {
+                    NoteId = detail.NoteId,
+                    Title = detail.Title,
+                    Content = detail.Content
+                };
+            return View(model);
+        }
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            return service;
+        }
+
     }
 }
